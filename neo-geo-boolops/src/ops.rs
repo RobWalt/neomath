2830c83a892f0e7eb6d1c109e835cfe219f13704
RobWalt <robwalter96@gmail.com>
2023-07-02 20:46:04 +0200

    feat: implement visualization of results of neo-geo-boolops

--- neo-geo-boolops/src/ops.rs
+++ neo-geo-boolops/src/ops.rs
@@ -39,11 +39,30 @@ pub fn union<F: NeoFloat>(
 
 #[cfg(test)]
 mod stress_tests {
+    use geo::Intersects;
+    use geo_svg::ToSvg;
     use std::f32::consts::TAU;
+    use std::io::Write;
 
     use rand::{thread_rng, Rng};
 
     use crate::ops::{difference, intersect};
+
+    fn save_svg(g: &impl ToSvg, path: &str, color: &'static str) {
+        let svg = g.to_svg();
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(format!("{path}.html"))
+            .unwrap();
+        _ = f.write(
+            svg.with_opacity(0.3)
+                .with_color(geo_svg::Color::Named(color))
+                .to_string()
+                .as_bytes(),
+        );
+    }
 
     fn make_circle(n: usize, mid: geo::Coord<f32>, radius: f32) -> geo::LineString<f32> {
         geo::LineString::<f32>::new(
@@ -61,29 +80,45 @@ mod stress_tests {
     fn make_random_poly() -> geo::MultiPolygon<f32> {
         let mut rng = thread_rng();
         let ext = make_circle(100, geo::Coord::zero(), 100.0);
-        let int = (1..=rng.gen_range(3..=6))
-            .map(|_| {
+        let mut int = vec![];
+        (1..=rng.gen_range(3..=6)).for_each(|i| {
+            while int.len() != i {
                 let mid = geo::Coord {
                     x: rng.gen_range(-50.0..=50.0),
                     y: rng.gen_range(-50.0..=50.0),
                 };
-                make_circle(20, mid, 20.0)
-            })
+                let c = geo::Polygon::new(make_circle(20, mid, 20.0), vec![]);
+                if int.iter().all(|o: &geo::Polygon<f32>| !o.intersects(&c)) {
+                    int.push(c);
+                }
+            }
+        });
+        let int = int
+            .into_iter()
+            .map(|i| i.exterior().clone())
             .collect::<Vec<_>>();
         geo::MultiPolygon::new(vec![geo::Polygon::<f32>::new(ext, int)])
     }
 
     #[test]
     fn random_intersect_test() {
+        _ = std::fs::remove_file("inter.html");
         let first = make_random_poly();
+        save_svg(&first, "inter", "blue");
         let second = make_random_poly();
-        let _x = intersect(&first, &second);
+        save_svg(&second, "inter", "red");
+        let x = intersect(&first, &second);
+        save_svg(&x, "inter", "black");
     }
 
     #[test]
     fn random_difference_test() {
+        _ = std::fs::remove_file("diff.html");
         let first = make_random_poly();
+        save_svg(&first, "diff", "blue");
         let second = make_random_poly();
-        let _x = difference(&first, &second);
+        save_svg(&second, "diff", "red");
+        let x = difference(&first, &second);
+        save_svg(&x, "diff", "black");
     }
 }
